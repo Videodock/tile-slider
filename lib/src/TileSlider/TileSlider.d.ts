14b82fa906041f78aacbd9920e00c61052a34cb1
Robin van Zanten <rcvzanten@gmail.com>
2022-05-18 09:49:16 +0200

    chore: update lib

--- lib/src/TileSlider/TileSlider.d.ts
+++ lib/src/TileSlider/TileSlider.d.ts
@@ -1,10 +1,10 @@
-import React from 'react';
+/// <reference types="react" />
 import './TileSlider.css';
 export declare const CYCLE_MODE_STOP = "stop";
 export declare const CYCLE_MODE_RESTART = "restart";
 export declare const CYCLE_MODE_ENDLESS = "endless";
 export declare type CycleMode = 'stop' | 'restart' | 'endless';
-export declare type RenderTile<T> = (item: T, isInView: boolean, listIndex: number, renderKey: string, slide: (direction: Direction) => void) => JSX.Element;
+export declare type RenderTile<T> = (item: T, isInView: boolean, listIndex: number, renderKey: string, slide?: (direction: Direction) => void) => JSX.Element;
 export declare type RenderControl = (props: ControlProps) => JSX.Element;
 export declare type ControlProps = {
     onClick: () => void;
@@ -30,15 +30,11 @@ export declare type TileSliderProps<T> = {
     renderRightControl?: RenderControl;
     passiveEventlistener?: boolean;
     renderPaginationDots?: (index: number, pageIndex: number) => JSX.Element;
-    renderAriaLabel?: (tile: Tile<T>, total: number) => string;
+    renderAriaLabel?: (item: T, index: number, key: string, total: number) => string;
     onSwipeStart?: () => void;
     onSwipeEnd?: () => void;
     onSlideEnd?: () => void;
+    overscan?: number;
 };
-declare type Tile<T> = {
-    item: T;
-    key: string;
-    index: number;
-};
-declare const TileSlider: React.ForwardRefExoticComponent<TileSliderProps<unknown> & React.RefAttributes<unknown>>;
+declare const TileSlider: <T extends unknown>({ items, tilesToShow, cycleMode, spacing, minimalTouchMovement, showControls, animated, transitionTime, transitionTimingFunction, wrapWithEmptyTiles, showDots, pageStep, renderTile, renderLeftControl, renderRightControl, renderPaginationDots, renderAriaLabel, className, onSwipeStart, onSwipeEnd, onSlideEnd, overscan, }: TileSliderProps<T>) => JSX.Element;
 export default TileSlider;
