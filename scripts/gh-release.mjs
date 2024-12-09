86ce3f6245aab2dd3e1c3058564a3e58aeb410c2
Christiaan Scheermeijer <christiaan@videodock.com>
2024-12-09 15:35:58 +0100

    chore: use package version

--- scripts/gh-release.mjs
+++ scripts/gh-release.mjs
@@ -1,10 +1,15 @@
 import child_process from 'node:child_process';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 import open from 'open';
 import createReleaseUrl from 'new-github-release-url';
 
 const output = child_process.execSync('npx conventional-changelog -p angular -r 2');
-const tag = child_process.execSync('git describe --tags --abbrev=0');
+
+const packageText = readFileSync(join(process.cwd(), 'package.json'));
+const packageJson = JSON.parse(packageText.toString());
+const tag = `v${packageJson.version}`;
 
 const releaseUrl = createReleaseUrl({
   repoUrl: 'https://github.com/Videodock/tile-slider',
